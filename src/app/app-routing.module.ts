ea386d04098ecd16d9b571dcccf16edac31996f2
ABHIJITH KUMAR <abhijithkumar09@gmail.com>
2021-11-15 14:18:47 +0530

    Inventory-app updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './error-routing/not-found/not-found.component';
 import { UncaughtErrorComponent } from './error-routing/error/uncaught-error.component';
 import { ErrorRoutingModule } from './error-routing/error-routing.module';
+import { ListComponent } from './list/list.component';
 import { CreateComponent } from './create/create.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'create', pathMatch: 'full' },
   { path: 'error', component: UncaughtErrorComponent },
+  { path: 'list', component: ListComponent, data: { text: 'list' } },
   { path: 'create', component: CreateComponent, data: { text: 'create' } },
   { path: '**', component: PageNotFoundComponent } // must always be last
 ];
