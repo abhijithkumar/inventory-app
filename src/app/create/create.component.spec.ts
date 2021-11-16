62e0455b3e9da31903807f6eb05b864db419a804
ABHIJITH KUMAR <abhijithkumar09@gmail.com>
2021-11-16 13:23:40 +0530

    Inventory-app updates

--- src/app/create/create.component.spec.ts
+++ src/app/create/create.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
-import { IgxInputGroupModule, IgxSelectModule } from 'igniteui-angular';
+import { IgxInputGroupModule, IgxSelectModule, IgxButtonModule } from 'igniteui-angular';
 import { CreateComponent } from './create.component';
 
 describe('CreateComponent', () => {
@@ -12,7 +12,7 @@ describe('CreateComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ CreateComponent ],
-      imports: [ NoopAnimationsModule, FormsModule, IgxInputGroupModule, IgxSelectModule ]
+      imports: [ NoopAnimationsModule, FormsModule, IgxInputGroupModule, IgxSelectModule, IgxButtonModule ]
     })
     .compileComponents();
   });
